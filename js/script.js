50e263f1425efed236196ed229c4cca5fb35f9f7
Vaibhav <86156611+imvbh@users.noreply.github.com>
2023-09-30 12:41:55 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-// menu icon bar
+
 let menuIcon = document.querySelector(".nav__toggle");
 let navbar = document.querySelector(".nav__menu");
 
@@ -109,3 +109,30 @@ scrollupButton.addEventListener('click', (e) => {
     behavior: 'smooth',
   });
 });
+
+
+/**
+ * SCROLL REVEAL
+ */
+
+const revealElements = document.querySelectorAll("[data-reveal]");
+const revealDelayElements = document.querySelectorAll("[data-reveal-delay]");
+
+const reveal = function () {
+  for (let i = 0, len = revealElements.length; i < len; i++) {
+    if (
+      revealElements[i].getBoundingClientRect().top <
+      window.innerHeight / 1.2
+    ) {
+      revealElements[i].classList.add("revealed");
+    }
+  }
+};
+
+for (let i = 0, len = revealDelayElements.length; i < len; i++) {
+  revealDelayElements[i].style.transitionDelay =
+    revealDelayElements[i].dataset.revealDelay;
+}
+
+window.addEventListener("scroll", reveal);
+window.addEventListener("load", reveal);
