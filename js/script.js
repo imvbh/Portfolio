40f2359e5308cd64f76e37724664f23dd540bfcb
Vaibhav <86156611+imvbh@users.noreply.github.com>
2024-07-09 17:09:50 +0530

    Custom Cursor

--- js/script.js
+++ js/script.js
@@ -136,3 +136,18 @@ for (let i = 0, len = revealDelayElements.length; i < len; i++) {
 
 window.addEventListener("scroll", reveal);
 window.addEventListener("load", reveal);
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+    cursor.setAttribute("style", "top: "+(e.pageY - 10)+"px; left: "+(e.pageX - 10)+"px;")
+})
+
+document.addEventListener('click', () => {
+    cursor.classList.add("expand");
+
+    setTimeout(() => {
+        cursor.classList.remove("expand");
+    }, 500)
+})
